0f6bc688ea924a74b2de2294715fa060fea2703c
Cody Snow <codysnow3@gmail.com>
2025-09-15 17:50:20 -0230

    Set up springboot for project, entry point working at the moment.

---
+++ src/main/java/com/KeyinDSA/AirportSprintReDo.java
@@ -0,0 +1,11 @@
+package com.KeyinDSA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirportSprintReDo {
+    public static void main(String[] args) {
+        SpringApplication.run(AirportSprintReDo.class, args);
+    }
+}
