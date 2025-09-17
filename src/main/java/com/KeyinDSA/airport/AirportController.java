edb41551076aa9adbd8c68c69cb182b7bc9e95d1
Cody Snow <codysnow3@gmail.com>
2025-09-17 18:20:02 -0230

    Added missing paths, and fixed request -> response

--- src/main/java/com/KeyinDSA/airport/AirportController.java
+++ src/main/java/com/KeyinDSA/airport/AirportController.java
@@ -1,4 +1,41 @@
 package com.KeyinDSA.airport;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+
 public class AirportController {
+
+    @Autowired
+    private AirportService airportService;
+
+    @GetMapping("/airports")
+    public List<Airport> getAllAirports() {
+        return airportService.getAllAirports();
+    }
+
+    @GetMapping("/airport/{id}")
+    public ResponseEntity<Airport> getAirportById(@PathVariable int id) {
+        Airport airport = airportService.getAirportById(id);
+        if (airport == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(airport);
+        }
+    }
+
+    @PostMapping("/airport")
+    public Airport createAirport(@RequestBody Airport airport) {
+        return airportService.createAirport(airport);
+    }
+
+    @DeleteMapping("/airport/{id}")
+    public void deleteAirportById(@PathVariable int id) {
+        airportService.deleteAirportById(id);
+    }
 }
